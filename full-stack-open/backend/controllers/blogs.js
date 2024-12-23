ba18d86a04d23a2e17c6b6a23f47ace3ee3c8c5c
nckvDev <jirapong.ku@rmuti.ac.th>
2024-12-23 13:37:21 +0700

    blog list expansions test and implement delete and update

--- full-stack-open/backend/controllers/blogs.js
+++ full-stack-open/backend/controllers/blogs.js
@@ -20,4 +20,23 @@ blogsRouter.post('/', async (request, response) => {
   response.status(201).json(saveBlog)
 })
 
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+
+  const blog = {
+    title: body.title,
+    author: body.author,
+    url: body.url,
+    likes: body.likes
+  }
+
+  const updateBlog = await Blog.findByIdAndUpdate(request.params.id, blog, {  new: true })
+  response.json(updateBlog)
+})
+
 module.exports = blogsRouter
