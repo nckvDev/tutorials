98f3513c9cfde40281d2edae9c376d7edb0b0f3a
nckvDev <jirapong.ku@rmuti.ac.th>
2024-12-23 12:20:10 +0700

    blog list test

--- full-stack-open/backend/tests/test_helper.js
+++ full-stack-open/backend/tests/test_helper.js
@@ -1,4 +1,5 @@
 const Note = require('../models/note')
+const Blog = require('../models/blog')
 
 const initialNotes = [
   {
@@ -8,6 +9,21 @@ const initialNotes = [
   {
     content: 'Browser can execute only JavaScript',
     important: true
+  }
+]
+
+const initialBlogs = [
+  {
+    title: 'React patterns',
+    author: 'Michael Chan',
+    url: 'https://reactpatterns.com/',
+    likes: 7,
+  },
+  {
+    title: 'Go To Statement Considered Harmful',
+    author: 'Edsger W. Dijkstra',
+    url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+    likes: 5,
   }
 ]
 
@@ -24,8 +40,15 @@ const noteInDb = async () => {
   return notes.map(note => note.toJSON())
 }
 
+const blogInDb = async () => {
+  const blogs = await Blog.find({})
+  return blogs.map(blog => blog.toJSON())
+}
+
 module.exports = {
   initialNotes,
   nonExistingId,
-  noteInDb
+  noteInDb,
+  initialBlogs,
+  blogInDb
 }
