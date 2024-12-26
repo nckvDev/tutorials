c5d49a3edc50ff728bd192368ef6302ad298c567
nckvDev <jirapong.ku@rmuti.ac.th>
2024-12-26 16:50:46 +0700

    Forwarding redux store to various component and more component

--- full-stack-open/state-management/src/App.jsx
+++ full-stack-open/state-management/src/App.jsx
@@ -1,19 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import NewNote from './components/NewNote'
+import Notes from './components/Notes'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <ul>
-        {store.getState().map(note=>
-          <li key={note.id}>
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
+      <NewNote />
+      <Notes />
     </div>
   )
 }
--- full-stack-open/state-management/src/main.jsx
+++ full-stack-open/state-management/src/main.jsx
@@ -1,106 +1,17 @@
-// import { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-// import App from './App.jsx'
 import { legacy_createStore as createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './App.jsx'
 import noteReducer from './reducers/noteReducer'
-
-const counterReducer = (state = 0, action) => {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    case 'ZERO':
-      return 0
-    default:
-      return state
-  }
-}
-
 
 const store = createStore(noteReducer)
 
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  payload: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-const generateId = () => Number((Math.random() * 1000000).toFixed(0))
-
-const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    payload: {
-      content,
-      important: false,
-      id: generateId()
-    }
-  }
-}
-
-const toggleImportanceOf = (id) => {
-  return {
-    type: 'TOGGLE_IMPORTANCE',
-    payload: { id }
-  }
-}
-
-const App = () => {
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    store.dispatch(createNote(content))
-  }
-
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id))
-  }
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input name='note' />
-        <button type='submit'>add</button>
-      </form>
-      <ul>
-        {store.getState().map(note=>
-          <li 
-            key={note.id}
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important' : ''}</strong>
-          </li>
-        )}
-        </ul>
-    </div>
-  )
-}
-
-const root = createRoot(document.getElementById('root'))
-
-const renderApp = () => {
-  root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
-
-// createRoot(document.getElementById('root')).render(
-//   <StrictMode>
-//     <App store={store} />
-//   </StrictMode>,
-// )
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+)
