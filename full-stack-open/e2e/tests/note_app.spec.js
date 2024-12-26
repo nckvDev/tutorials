769a78c2b52f8bf59ca215af0fc9db926c2bdca7
nckvDev <jirapong.ku@rmuti.ac.th>
2024-12-26 12:24:46 +0700

    controlling the state of the database

--- full-stack-open/e2e/tests/note_app.spec.js
+++ full-stack-open/e2e/tests/note_app.spec.js
@@ -1,7 +1,16 @@
-const { test, expect } = require("@playwright/test");
+const { test, expect } = require('@playwright/test')
 
 test.describe('Note app', () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async ({ page, request }) => {
+    await request.post('http://localhost:3001/api/testing/reset')
+    await request.post('http://localhost:3001/api/users', {
+      data: {
+        name: 'Matti Luukkainen',
+        username: 'mluukkai',
+        password: 'salainen'
+      }
+    })
+
     await page.goto('http://localhost:5173')
   })
 
@@ -19,37 +28,37 @@ test.describe('Note app', () => {
       both are problematic to the extent that if the registration form is changed, 
       the tests may break, as they rely on the fields to be on the page in a certain order. 
     */
-    // await page.getByRole('textbox').first().fill('root')
+    // await page.getByRole('textbox').first().fill('mluukkai')
     // await page.getByRole('textbox').last().fill('salainen')
 
     // const textBoxes = await page.getByRole('textbox').all()
-    // await textBoxes[0].fill('root')
+    // await textBoxes[0].fill('mluukkai')
     // await textBoxes[1].fill('salainen')
 
     /*
       A better solution is to define unique test id attributes for the fields, 
       to search for them in the tests using the method getByTestId.
     */
-    await page.getByTestId('username').fill('root')
+    await page.getByTestId('username').fill('mluukkai')
     await page.getByTestId('password').fill('salainen')
 
     await page.getByRole('button', { name: 'login' }).click()
 
-    await expect(page.getByText('Superuser logged-in')).toBeVisible()
+    await expect(page.getByText('Matti Luukkainen logged-in')).toBeVisible()
   })
 
   test('user can log in', async ({ page }) => {
     await page.getByRole('button', { name: 'log in' }).click()
-    await page.getByTestId('username').fill('root')
+    await page.getByTestId('username').fill('mluukkai')
     await page.getByTestId('password').fill('salainen')
     await page.getByRole('button', { name: 'login' }).click()
-    await expect(page.getByText('Superuser logged-in')).toBeVisible()
+    await expect(page.getByText('Matti Luukkainen logged-in')).toBeVisible()
   })
 
   test.describe('when logged in', () => {
     test.beforeEach(async ({ page }) => {
       await page.getByRole('button', { name: 'log in' }).click()
-      await page.getByTestId('username').fill('root')
+      await page.getByTestId('username').fill('mluukkai')
       await page.getByTestId('password').fill('salainen')
       await page.getByRole('button', { name: 'login' }).click()
     })
@@ -59,6 +68,19 @@ test.describe('Note app', () => {
       await page.getByRole('textbox').fill('a note created by playwright')
       await page.getByRole('button', { name: 'save' }).click()
       await expect(page.getByText('a note created by playwright')).toBeVisible()
+    })
+
+    test.describe('and a note exists', () => {
+      test.beforeEach(async ({ page }) => {
+        await page.getByRole('button', { name: 'new note' }).click()
+        await page.getByRole('textbox').fill('another note by playwright')
+        await page.getByRole('button', { name: 'save' }).click()
+      })
+
+      test('importance can be changed', async ({ page }) => {
+        await page.getByRole('button', { name: 'make not important' }).click()
+        await expect(page.getByText('make important')).toBeVisible()
+      })
     })
   })
 })
