9dc083ab7319bb445ca7e6338ced663c3eddf46e
nckvDev <jirapong.ku@rmuti.ac.th>
2025-01-07 17:51:21 +0700

    Exc Anecdotes and Hooks

--- full-stack-open/routed/src/hooks/index.js
+++ full-stack-open/routed/src/hooks/index.js
@@ -1,16 +1,21 @@
 import { useState } from "react"
 
 export const useField = (type) => {
-  const [value, setValue] = useState()
+  const [value, setValue] = useState('')
 
   const onChange = (event) => {
     setValue(event.target.value)
+  }
+
+  const onReset = () => {
+    setValue('')
   }
   
   return {
     type,
     value,
-    onChange
+    onChange,
+    onReset
   }
 }
 
